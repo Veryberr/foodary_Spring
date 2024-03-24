f210639f29aac9678a7525895a26a2266e2ed191
Veryberr <raincloud0515@naver.com>
2024-03-25 00:48:19 +0900

    [fix] target 폴더에 commit(2)

---
+++ src/main/java/com/foodary/config/AppConfig.java
@@ -0,0 +1,84 @@
+//package com.foodary.config;
+//
+//import com.foodary.diet.vo.DietList;
+//import com.foodary.diet.vo.DietVO;
+//import com.foodary.food.vo.FoodList;
+//import com.foodary.food.vo.FoodVO;
+//import com.foodary.freeboard.commentVO.FreeboardCommentList;
+//import com.foodary.freeboard.commentVO.FreeboardCommentVO;
+//import com.foodary.freeboard.vo.FreeboardList;
+//import com.foodary.freeboard.vo.FreeboardVO;
+//import com.foodary.user.vo.UserRegisterList;
+//import com.foodary.user.vo.UserRegisterVO;
+//import com.foodary.vo.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@ComponentScan( basePackages = "com.foodary" )
+//public class AppConfig {
+//
+//    @Bean
+//    public FreeboardVO freeboardVO() {
+//        return new FreeboardVO();
+//    }
+//
+//    @Bean
+//	public FreeboardList freeboardList() {
+//		return new FreeboardList();
+//	}
+//
+//	@Bean
+//	public FreeboardCommentVO freeboardCommentVO() {
+//		return new FreeboardCommentVO();
+//	}
+//
+//	@Bean
+//	public FreeboardCommentList freeboardCommentList() {
+//		return new FreeboardCommentList();
+//	}
+//
+//	@Bean
+//	public UserRegisterVO userRegisterVO() {
+//		return new UserRegisterVO();
+//	}
+//
+//	@Bean
+//	public UserRegisterList userRegisterList() {
+//		return new UserRegisterList();
+//	}
+//
+//	@Bean
+//	public DietVO dietVO() {
+//		return new DietVO();
+//	}
+//
+//	@Bean
+//	public DietList dietList() {
+//		return new DietList();
+//	}
+//
+//	@Bean
+//	public FoodVO foodVO() {
+//		return new FoodVO();
+//	}
+//
+//	@Bean
+//	public FoodList foodList() {
+//		return new FoodList();
+//	}
+//
+//	@Bean
+//	public UserFoodVO userFoodVO() {
+//		return new UserFoodVO();
+//	}
+//
+//	@Bean
+//	public UserFoodList userFoodList() {
+//		return new UserFoodList();
+//	}
+//
+//
+//
+//}
